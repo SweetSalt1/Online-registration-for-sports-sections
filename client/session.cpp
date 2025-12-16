f6e250c3586ddcdcee6c66f9017e7a0ff834c05d
SweetSalt1 <appleappolov@gmail.com>
2025-12-16 23:55:58 +0300

    Update session.cpp

--- client/session.cpp
+++ client/session.cpp
@@ -1,2 +1,18 @@
+#pragma once
 #include "session.h"
+#include <string>
 
+class Session {
+public:
+    void SetToken(const std::string& t) { token_ = t; }
+    void SetRole(const std::string& r) { role_ = r; }
+
+    const std::string& GetToken() const { return token_; }
+    const std::string& GetRole() const { return role_; }
+
+    bool IsLogged() const { return !token_.empty(); }
+
+private:
+    std::string token_;
+    std::string role_;
+};
