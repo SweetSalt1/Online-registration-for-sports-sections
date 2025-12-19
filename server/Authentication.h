7eacccff282fbf49be16ab6d47c64c140a13ba2e
Yaroslav <vrubskijj@gmail.com>
2025-12-19 16:03:14 +0300

    Update Authentication.h

--- server/Authentication.h
+++ server/Authentication.h
@@ -3,25 +3,15 @@
 
 class Authentication {
 public:
-
     ConnectBD startserver("tcp://127.0.0.1:3306","user","123Bd321!","mybd");
-
     Authentication(std::string& login, std::string& password,std::string& role,std::string& new_password);
-
     bool RegistrationUser(const std::string& login, const std::string& password, const std::string& role);
-
     bool SignUser(const std::string& login, const std::string& password);
-
     bool ReplaceInfo(const std::string& login, const std::string& password, const std::string& new_password);
-
     bool DeleteUser(const std::string&login);
-
     crow::json::wvalue GetAllUsers();
-
     void Unpack(const crow::json::rvalue& json);
-    
     crow::json::wvalue Pack() const;
-
 private:
     std::string login_;
     std::string password_;
