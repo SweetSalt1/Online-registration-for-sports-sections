c838a2965a947a8502ffe7d8fa42d1ae9e26fa56
Yaroslav <vrubskijj@gmail.com>
2025-12-19 15:50:12 +0300

    Update Startserver.h

--- server/Startserver.h
+++ server/Startserver.h
@@ -1,46 +1,15 @@
 #pragma once
 
 #include "Authentication.h"
-#include "Student.h"
-#include "Secretary.h"
-#include "Session.h"
+
+//#include "Student.h"
+//#include "Secretary.h"
+//#include "Session.h"
 class StartServer
 {
   public:
-  Authentication auth("user","123","Secretary","321");
-  Student student(1);
-
-  crow::SimpleApp app;
-
-
-  StartServer(int port_);
-
-  crow::response HandleJSONRequest(const crow::request& req);
-      
-
-    CROW_ROUTE(app, "/register").methods("POST"_method)
-    ([](const crow::request& req) {   });
-    
-    CROW_ROUTE(app, "/login").methods("POST"_method)
-    ([](const crow::request& req) { });
-
-    CROW_ROUTE(app, "/replace/password").methods("POST"_method)
-    ([](const crow::request& req) { });
-
-    CROW_ROUTE(app, "/insert/information/student").methods("POST"_method)
-    ([](const crow::request& req) {  });
-
-    CROW_ROUTE(app, "/insert/application/student").methods("POST"_method)
-    ([](const crow::request& req) {  });
-    
-    CROW_ROUTE(app, "/get/section/student").methods("POST"_method)
-    ([](const crow::request& req) {   });
-
-    CROW_ROUTE(app, "/get/myapplication/student").methods("POST"_method)
-    ([](const crow::request& req) {  });
-
-  void Start_server();
-
+StartServer(int port_);
+void Start_server();
   private:
   int port;
 }
