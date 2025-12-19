242f48995b8959f154ec611322cc61f359868c1f
Yaroslav <vrubskijj@gmail.com>
2025-12-19 08:23:24 +0300

    Update ConnectBD.cpp

--- server/ConnectBD.cpp
+++ server/ConnectBD.cpp
@@ -2,18 +2,29 @@
 class ConnectBD
 {
     public:
-    std::unique_ptr<sql::Connection> getConnection()
+    ConnectBD(const std::string& server, const std::string& username, 
+              const std::string& password, const std::string& database)
+        : serverSQL(server), usernameSQL(username), 
+          passwordSQL(password), databaseSQL(database)
     {
         driver = sql::mysql::get_mysql_driver_instance();
-        std::unique_ptr<sql::Connection> conn(driver->connect(serverSQL, usernameSQL, passwordSQL));
-        conn->setSchema(databaseSQL);
+    }
+    std::unique_ptr<sql::Connection> getConnection()
+    {
+        std::unique_ptr<sql::Connection> conn(driver->connect(serverSQL_, usernameSQL_, passwordSQL_));
+        conn->setSchema(databaseSQL_);
         return conn;
     }
 
     private:
     sql::mysql::MySQL_Driver* driver;
-    const std::string serverSQL = "tcp://127.0.0.1:3306";
-    const std::string usernameSQL = "user";
-    const std::string passwordSQL = "123Bd321!";
-    const std::string databaseSQL = "mybd";
+    const std::string serverSQL_ = "tcp://127.0.0.1:3306";
+    const std::string usernameSQL_ = "user";
+    const std::string passwordSQL_ = "123Bd321!";
+    const std::string databaseSQL_ = "mybd";
+
+    std::string serverSQL;
+    std::string usernameSQL;
+    std::string passwordSQL;
+    std::string databaseSQL;
 }
