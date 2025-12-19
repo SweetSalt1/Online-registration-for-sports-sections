d4b7bd014000705b801820cb6f90ec725f35bad6
Yaroslav <vrubskijj@gmail.com>
2025-12-19 08:22:55 +0300

    Update ConnectBD.h

--- server/ConnectBD.h
+++ server/ConnectBD.h
@@ -1,12 +1,20 @@
+#pragma once
 #include <iostream>
 #include <mysql/jdbc.h>
 #include <mutex>
 #include <queue>
 #include <list>
 #include <memory>
+#include <string>
+#include <vector>
 class ConnectBD
 {
     public:
+    ConnectBD(const std::string& server, const std::string& username, 
+              const std::string& password, const std::string& database)
+        : serverSQL(server), usernameSQL(username), 
+          passwordSQL(password), databaseSQL(database);
+          
     std::unique_ptr<sql::Connection> getConnection();
     
     private:
@@ -15,4 +23,9 @@ class ConnectBD
     const std::string usernameSQL;
     const std::string passwordSQL;
     const std::string databaseSQL;
+
+    std::string serverSQL;
+    std::string usernameSQL;
+    std::string passwordSQL;
+    std::string databaseSQL;
 }
