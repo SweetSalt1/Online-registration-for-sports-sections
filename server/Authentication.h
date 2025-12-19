3a6efded10332bad883cd1022abaa05c3ed77242
Yaroslav <vrubskijj@gmail.com>
2025-12-19 15:50:59 +0300

    Update Authentication.h

--- server/Authentication.h
+++ server/Authentication.h
@@ -3,12 +3,20 @@
 
 class Authentication {
 public:
-    ConnectBD startserver("tcp://127.0.0.1:3306","user","123Bd321!","mybd");
-    Authentication(std::string& login, std::string& password,std::string& role,std::string& new_password);
-    bool RegistrationUser(const std::string& login_, const std::string& password_, const std::string& role_);
-    bool SignUser(const std::string& login_, const std::string& password_);
 
-    bool ReplaceInfo(const std::string& login_, const std::string& password_, const std::string& new_password_);
+    ConnectBD startserver("tcp://127.0.0.1:3306","user","123Bd321!","mybd");
+
+    Authentication(std::string& login, std::string& password,std::string& role,std::string& new_password);
+
+    bool RegistrationUser(const std::string& login, const std::string& password, const std::string& role);
+
+    bool SignUser(const std::string& login, const std::string& password);
+
+    bool ReplaceInfo(const std::string& login, const std::string& password, const std::string& new_password);
+
+    bool DeleteUser(const std::string&login);
+
+    crow::json::wvalue GetAllUsers();
 
     void Unpack(const crow::json::rvalue& json);
     
@@ -18,6 +26,6 @@ private:
     std::string login_;
     std::string password_;
     std::string role_;
-    std::string new_password;
+    std::string new_password_;
     int student_id_=1;
 };
