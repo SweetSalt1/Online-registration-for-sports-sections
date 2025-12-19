e7f8c1c7a30ab9e2ac3399c3f2f94d8001deda71
Yaroslav <vrubskijj@gmail.com>
2025-12-19 15:57:59 +0300

    Update int_main.cpp

--- server/int_main.cpp
+++ server/int_main.cpp
@@ -1,7 +1,6 @@
 #include "Startserver.h"
 
-int main()
-{
+int main() {
     ConnectBD startserver("tcp://127.0.0.1:3306","user","123Bd321!","mybd");
     Authentication auth("user","123","Secretary","321");
     StartServer start(8080);
