6eabb173a4a3953663e45d7a52f344118c4f72fb
Yaroslav <vrubskijj@gmail.com>
2025-12-19 15:35:57 +0300

    Update int_main.cpp

--- server/int_main.cpp
+++ server/int_main.cpp
@@ -2,6 +2,8 @@
 
 int main()
 {
+    ConnectBD startserver("tcp://127.0.0.1:3306","user","123Bd321!","mybd");
+    Authentication auth("user","123","Secretary","321");
     StartServer start(8080);
     start.Start_server();
 }
